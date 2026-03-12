1d241fe6e2a1ec906e946fa2c5023b9d4f96409a
govind904 <govindu2003@gmail.com>
2026-03-12 16:23:26 +0530

    Add files via upload

---
+++ StudentGradeTracker.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+public class StudentGradeTracker {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+        int[] marks = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter marks for student " + (i + 1) + ": ");
+            marks[i] = sc.nextInt();
+        }
+
+        int total = 0;
+        int highest = marks[0];
+        int lowest = marks[0];
+
+        for (int i = 0; i < n; i++) {
+
+            total += marks[i];
+
+            if (marks[i] > highest) {
+                highest = marks[i];
+            }
+
+            if (marks[i] < lowest) {
+                lowest = marks[i];
+            }
+        }
+
+        double average = (double) total / n;
+
+        System.out.println("\n----- Student Report -----");
+        System.out.println("Average Marks: " + average);
+        System.out.println("Highest Marks: " + highest);
+        System.out.println("Lowest Marks: " + lowest);
+    }
+}
